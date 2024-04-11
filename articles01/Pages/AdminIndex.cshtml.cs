adc08f643cbdbf9efd73ed0bb5a7d0901f63ccc4
hp <hp@ITMANAGER>
2024-04-11 14:55:14 +0200

    user's profile page and statistics

--- articles01/Pages/AdminIndex.cshtml.cs
+++ articles01/Pages/AdminIndex.cshtml.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using articles01.Data;
+using articles01.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,8 +14,25 @@ namespace articles01.Pages
     [Authorize]
     public class AdminIndexModel : PageModel
     {
+
+         private readonly IDataHelper<AuthorPost> dataHelper;
+        public int AllPosts { get; set; }
+        public int PostsLastMonth { get; set; }
+        public int PostThisYear { get; set; }
+        public AdminIndexModel(IDataHelper<AuthorPost> dataHelper)
+        {
+            this.dataHelper = dataHelper;
+        }
         public void OnGet()
         {
+            var userid = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var datem = DateTime.Now.AddMonths(-1);
+            var datey = DateTime.Now.AddYears(-1);
+
+            AllPosts = dataHelper.GetDataByUser(userid).Count();
+            PostsLastMonth = dataHelper.GetDataByUser(userid).Where(x => x.AddedDate >= datem).Count();
+            PostThisYear = dataHelper.GetDataByUser(userid).Where(x => x.AddedDate >= datey).Count();
+
         }
     }
 }
