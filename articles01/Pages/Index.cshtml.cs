95cacb9914b540a5c7a9c486f897d5ffa80bcdc4
hp <hp@ITMANAGER>
2024-04-11 17:51:37 +0200

    category filtering

--- articles01/Pages/Index.cshtml.cs
+++ articles01/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using articles01.Data;
+using articles01.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,14 +13,58 @@ namespace articles01.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IDataHelper<Category> dataHelperCategory;
+        private readonly IDataHelper<AuthorPost> dataHelperAuthorPost;
+        private readonly int ItemsNum;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IDataHelper<Category> dataHelperCategory,
+            IDataHelper<AuthorPost> dataHelperAuthorPost)
         {
             _logger = logger;
+            this.dataHelperCategory = dataHelperCategory;
+            this.dataHelperAuthorPost = dataHelperAuthorPost;
+            ItemsNum = 6;
+
+            ListForCategory = new List<Category>();
+            ListForPost = new List<AuthorPost>();
         }
 
-        public void OnGet()
+
+        public List<Category> ListForCategory { get; set; }
+        public List<AuthorPost> ListForPost { get; set; }
+
+
+        public void OnGet(string LoadState, string categoryName)
         {
+            GetAllCategory();
+
+            if (LoadState == null || LoadState == "All")
+            {
+                GetAllPosts();
+
+            }
+            else if (LoadState == "ByCategory")
+            {
+                GetDataByCategoryName(categoryName);
+            }
+        }
+
+        //get for category
+        private void GetAllCategory()
+        {
+            ListForCategory = dataHelperCategory.GetAllData();
+
+        }
+
+        private void GetAllPosts()
+        {
+            ListForPost = dataHelperAuthorPost.GetAllData().Take(ItemsNum).ToList();
+
+        }
+
+        private void GetDataByCategoryName(string categoryName)
+        {
+            ListForPost = dataHelperAuthorPost.GetAllData().Where(x=>x.PostCategory==categoryName).Take(ItemsNum).ToList();
 
         }
     }
